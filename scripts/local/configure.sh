6c053c0a1206e43f9d26799ef63a84f0e71eca53
Matt Kieblesz <mattkieblesz@hotmail.com>
2017-03-17 14:46:25 +0000

    Auto create vagrant files for all services

--- scripts/local/configure.sh
+++ scripts/local/configure.sh
@@ -41,8 +41,7 @@ main() {
 
         # run configuration playbook if present
         if [ -f $configure_playbook ]; then
-            ANSIBLE_CONFIG=$SCRIPT_DIR/../conf/ansible.cfg ansible-playbook $configure_playbook \
-                --extra-vars="env=$env_name"
+            ANSIBLE_CONFIG=$BASE_DIR/conf/ansible.cfg ansible-playbook $configure_playbook --extra-vars="env_name=$env_name"
         fi
 
         # sync store to remote
