1621c658e5a4da72c210416ac16e87c0bee676f1
Matt Kieblesz <mattkieblesz@hotmail.com>
2017-03-19 15:09:42 +0000

    Setup sudoers

--- scripts/local/configure.sh
+++ scripts/local/configure.sh
@@ -6,7 +6,6 @@ readonly BASE_DIR=$( cd $SCRIPT_DIR/.. && pwd )
 readonly WORKSPACE_DIR="$HOME/Projects"
 readonly AWS_CONFIG_DIR="$HOME/.aws"
 readonly AWS_CONFIG_FILE_LINK="$AWS_CONFIG_DIR/config"
-readonly ANSIBLE_CONFIG_FILE_LINK="$HOME/.ansible.cfg"
 readonly ANSIBLE_VAULT_PASS_FILE_LINK="$HOME/vpass"
 
 readonly STORE_BUCKET_NAME=$( cd $BASE_DIR && basename $(pwd) )
@@ -29,10 +28,7 @@ main() {
         ln -s $BASE_DIR/conf/aws-config $AWS_CONFIG_FILE_LINK
     fi
 
-    inf "--> Setup Ansible config"
-    # if [ ! -f $ANSIBLE_CONFIG_FILE_LINK ]; then
-    #     ln -s $BASE_DIR/conf/ansible.cfg $ANSIBLE_CONFIG_FILE_LINK
-    # fi
+    inf "--> Setup Ansible configs"
     if [ ! -f $ANSIBLE_VAULT_PASS_FILE_LINK ]; then
         ln -s $BASE_DIR/conf/vpass $ANSIBLE_VAULT_PASS_FILE_LINK
     fi
@@ -42,28 +38,24 @@ main() {
         /usr/local/bin/aws s3 mb s3://$STORE_BUCKET_NAME
     fi
 
+    inf "--> Configure local environment"
+    ANSIBLE_CONFIG=$BASE_DIR/conf/ansible.cfg ansible-playbook $SCRIPT_DIR/local/configure.yml
+
     for dir in envs/*/;  # list all dirs in envs directory
     do
         dir=${dir%/}  # strip trailing slash
         env_name="${dir#envs/}" # strip leading prefix
         env_dir="$BASE_DIR/envs/$env_name"
         store_dir="$env_dir/store"
-        configure_playbook="$env_dir/configure.yml"
 
         inf "--> Configure $env_name store"
 
         # create local store dirs
-        mkdir -p $store_dir/images $store_dir/backups $store_dir/services
-
-        # run configuration playbook if present
-        if [ -f $configure_playbook ]; then
-            ANSIBLE_CONFIG=$BASE_DIR/conf/ansible.cfg ansible-playbook $configure_playbook --extra-vars="base_dir=$BASE_DIR"
-        fi
+        mkdir -p $store_dir/backups $store_dir/services
 
         # sync store to remote
-        /usr/local/bin/aws s3 cp $store_dir s3://$STORE_BUCKET_NAME/$env_name --recursive --exclude '*/.vagrant/*'
+        # /usr/local/bin/aws s3 cp $store_dir s3://$STORE_BUCKET_NAME/$env_name --recursive --exclude '*/.vagrant/*'
     done
-
 }
 
 [[ "$0" == "$BASH_SOURCE" ]] && main
